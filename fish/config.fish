974a0d443b2278d10a3f33a013eed8058b99b589
the-die <xiulieshan@gmail.com>
2022-12-24 22:04:10 +0800

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,11 @@
 set -x EDITOR nvim
+set -x PATH "$HOME/.cargo/bin" $PATH
 
 alias nv=nvim
 alias ls=exa
 alias ll="exa -l"
-alias grep=egrep
+alias grep="egrep --color=auto"
 
 starship init fish | source
 zoxide init fish | source
+mcfly init fish | source
