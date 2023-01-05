50777bed60c73ea292be5d96ccb07e52fcb6d531
the-die <xiulieshan@gmail.com>
2023-01-05 19:52:50 +0800

    Update nvim config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
     options = {
         icons_enabled = false,
-        theme = 'auto',
+        theme = 'dracula',
         component_separators = {
             left = '>',
             right = '<'
