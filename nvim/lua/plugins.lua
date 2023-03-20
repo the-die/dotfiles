68c05de2ce7ef6b3994a49885a5103cff18a7699
the-die <xiulieshan@gmail.com>
2023-03-20 22:07:46 +0800

    Update nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -198,6 +198,7 @@ return {
   {
     'EdenEast/nightfox.nvim',
     lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('config.theme')
     end
