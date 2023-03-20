c5e5fd7e851c61791e74c4aba734c3f851a374c2
the-die <xiulieshan@gmail.com>
2023-03-20 12:23:14 +0800

    Update nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,145 +1,201 @@
-local filetypes = require'main'.lsp_filetypes
+local filetypes = require('main').lsp_filetypes
 
-return {{
-    "nvim-lua/plenary.nvim",
-    lazy = true
-}, {
-    "nvim-tree/nvim-web-devicons",
-    lazy = true
-}, {
-    'williamboman/mason.nvim',
-    config = function()
-        require('mason').setup()
-    end
-}, {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {'nvim-lua/plenary.nvim'},
-    init = function()
-        require('mappings').telescope()
-    end
-}, {
-    'nvim-tree/nvim-tree.lua',
-    config = function()
-        require('nvim-tree').setup {}
-    end,
-    init = function()
-        require('mappings').nvimtree()
-    end
-}, {
+return {
+  {
     'neovim/nvim-lspconfig',
     ft = filetypes,
     config = function()
-        require('config.lsp')
-    end
-}, {
-    "glepnir/lspsaga.nvim",
-    event = "BufRead",
+      require('config.lsp')
+    end,
+  },
+
+  {
+    'glepnir/lspsaga.nvim',
+    event = 'BufRead',
     init = function()
-        require'mappings'.lspsaga()
+      require('mappings').lspsaga()
     end,
     config = function()
-        require("lspsaga").setup({})
+      require('lspsaga').setup({})
     end,
-    dependencies = {{"nvim-tree/nvim-web-devicons"}, -- Please make sure you install markdown and markdown_inline parser
-    {"nvim-treesitter/nvim-treesitter"}}
-}, {
-    'mhartington/formatter.nvim',
-    ft = filetypes,
-    cmd = 'Format',
-    init = function()
-        require'mappings'.formatter()
-    end,
-    config = function()
-        require 'config.format'
-    end
-}, {
-    'j-hui/fidget.nvim',
-    config = function()
-        require'fidget'.setup {
-            text = {
-                spinner = 'dots_negative'
-            }
-        }
-    end
-}, {
+    dependencies = {
+      {'nvim-tree/nvim-web-devicons'},
+      -- Please make sure you install markdown and markdown_inline parser
+      {'nvim-treesitter/nvim-treesitter'},
+    },
+  },
+
+  {
     'ray-x/lsp_signature.nvim',
     config = function()
-        require 'config.lsp_signature'
-    end
-}, {
-    'akinsho/bufferline.nvim',
+      require('config.lsp_signature')
+    end,
+  },
+
+  {
+    'j-hui/fidget.nvim',
+    config = function()
+      require('fidget').setup({
+        text = {
+          spinner = 'dots_negative',
+        },
+      })
+    end,
+  },
+
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end,
+  },
+
+  {
+    'folke/trouble.nvim',
     init = function()
-        require'mappings'.bufferline()
+      require('mappings').trouble()
     end,
     config = function()
-        require 'config.bufferline'
-    end
-}, {
+      require('config.trouble')
+    end,
+  },
+
+  {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     config = function()
-        require 'config.cmp'
-    end
-}, {'L3MON4D3/LuaSnip'}, {'saadparwaiz1/cmp_luasnip'}, {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-path'},
+      require('config.cmp')
+    end,
+  },
 
-        {'hrsh7th/cmp-buffer'}, {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-        require 'config.lualine'
-    end
-}, {
+  {'hrsh7th/cmp-nvim-lsp',},
+
+  {'hrsh7th/cmp-path',},
+
+  {'hrsh7th/cmp-buffer',},
+
+  {
     'nvim-treesitter/nvim-treesitter',
     ft = filetypes,
     build = ':TSUpdate',
     event = 'BufRead',
     config = function()
-        require 'config.nvim-treesitter'
-    end
-}, {
+      require('config.nvim-treesitter')
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+  },
+
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require('nvim-surround').setup({
+        -- Configuration here, or leave empty to  defaults
+      })
+    end,
+  },
+
+
+  {'L3MON4D3/LuaSnip',},
+
+  {'saadparwaiz1/cmp_luasnip',},
+
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {'nvim-lua/plenary.nvim',},
+    init = function()
+      require('mappings').telescope()
+    end,
+  },
+
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      require('nvim-tree').setup({})
+    end,
+    init = function()
+      require('mappings').nvimtree()
+    end,
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('config.lualine')
+    end,
+  },
+
+  {
+    'akinsho/bufferline.nvim',
+    init = function()
+      require('mappings').bufferline()
+    end,
+    config = function()
+      require('config.bufferline')
+    end,
+  },
+
+  {
+    'nvim-tree/nvim-web-devicons',
+    lazy = true,
+  },
+
+  {
+    'nvim-lua/plenary.nvim',
+    lazy = true,
+  },
+
+  {
+    'ggandor/leap.nvim',
+    config = function()
+      require('leap').add_default_mappings()
+    end,
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup({})
+    end,
+  },
+
+  {
     'nvim-treesitter/nvim-treesitter-context',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = function()
-        require 'config.nvim-treesitter-context'
-    end
-}, {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = 'nvim-treesitter/nvim-treesitter'
-}, {
-    'kylechui/nvim-surround',
-    version = '*', --  for stability; omit to  `main` branch for the latest features
-    config = function()
-        require('nvim-surround').setup({
-            -- Configuration here, or leave empty to  defaults
-        })
-    end
-}, {
-    'windwp/nvim-autopairs',
-    config = function()
-        require('nvim-autopairs').setup {}
-    end
-}, {
-    'folke/trouble.nvim',
+      require('config.nvim-treesitter-context')
+    end,
+  },
+
+  {
+    'mhartington/formatter.nvim',
+    ft = filetypes,
+    cmd = 'Format',
     init = function()
-        require'mappings'.trouble()
+      require('mappings').formatter()
     end,
     config = function()
-        require 'config.trouble'
-    end
-}, {
+      require('config.format')
+    end,
+  },
+
+  {
     'lukas-reineke/indent-blankline.nvim',
     lazy = false,
     config = function()
-        require 'config.indent'
-    end
-}, {
-    'ggandor/leap.nvim',
-    config = function()
-        require'leap'.add_default_mappings()
-    end
-}, {
+      require('config.indent')
+    end,
+  },
+
+  {
     'EdenEast/nightfox.nvim',
     lazy = false,
     config = function()
-        require 'config.theme'
-    end
-}}
+      require('config.theme')
+    end,
+  },
+}
