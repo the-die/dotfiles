96b33a1cfb64104bf1bc945b2ebedcdba8c28751
the-die <xiulieshan@gmail.com>
2023-11-12 12:18:41 +0800

    Add v2ray and remove clash

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -43,18 +43,13 @@ return {
       require('config.cmp')
     end,
     dependencies = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
     },
   },
-
-  { 'hrsh7th/cmp-nvim-lsp' },
-
-  { 'hrsh7th/cmp-path' },
-
-  { 'hrsh7th/cmp-buffer' },
 
   {
     'nvim-treesitter/nvim-treesitter',
@@ -80,10 +75,6 @@ return {
       })
     end,
   },
-
-  { 'L3MON4D3/LuaSnip' },
-
-  { 'saadparwaiz1/cmp_luasnip' },
 
   {
     'nvim-telescope/telescope.nvim',
