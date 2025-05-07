c05273db9b9613cbf0e6a279d98d18b7aa9b9760
the-die <xiulieshan@gmail.com>
2025-05-07 21:35:17 +0800

    update fish and kitty config

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,12 @@ set -x EDITOR nvim
 set -x PATH "$HOME/.local/bin" $PATH
 set -x PATH "$HOME/go/bin" $PATH
 set -x PATH "$HOME/.cargo/bin" $PATH
+set -x GTK_IM_MODULE fcitx
+set -x QT_IM_MODULE fcitx
+set -x XMODIFIERS "@im=fcitx"
 
 alias nv=nvim
+alias cat=bat
 alias ls=exa
 alias ll="exa -lgH"
 alias grep="grep -E --color=auto"
